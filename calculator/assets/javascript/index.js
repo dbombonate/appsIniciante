a840fed30db806b9c247f1183767ed48f78dadba
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2021-04-23 16:44:12 -0300

    basic functions script

--- calculator/assets/javascript/index.js
+++ calculator/assets/javascript/index.js
@@ -1,5 +1,36 @@
-import App from "./app";
+function calc(){
+    const numbers = document.querySelectorAll('.number');
+    const operators = document.querySelectorAll('.operator');
+    const allClear = document.querySelector('.app-button-all-clear');
 
-const app = new App;
+    const displayCalc = document.querySelector('.app-display-operations');
+    const displayOperator = document.querySelector('.app-display-operator');
+    const displayResult = document.querySelector('.app-display-up');
+    
+    numbers.forEach((button) => {
+        button.addEventListener('click', insertNumber);
+    });
 
-app();
+    operators.forEach((operator) => {
+        operator.addEventListener('click', insertOperator);
+    });
+
+    allClear.addEventListener('click', allClean);
+
+    function insertNumber(e) {
+        displayCalc.innerHTML += e.target.value;
+    };
+
+    function insertOperator(e) {
+        let operator = e.target.value;
+        displayOperator.innerHTML = operator;
+    };
+
+    function allClean() {
+        displayCalc.innerHTML = '';
+        displayOperator.innerHTML = '';
+        displayResult.innerHTML = '';
+    }
+}
+
+calc();
