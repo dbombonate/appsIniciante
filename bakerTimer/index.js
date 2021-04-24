29e7bdd9b565a35be0039cbdcd7272cad304dab9
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2021-04-24 14:43:37 -0300

    Function to get baked bread

--- bakerTimer/index.js
+++ bakerTimer/index.js
@@ -1,9 +1,9 @@
 /*
- * Pegar horário da próxima fornada
- * Comparar com a hora atual
- *
- * Criar timer regressivo até a próxima fornada
- * Tocar alarme quando fornada pronta por 5s e a cada 30s por 2 minutos
+ * Receber as fornadas do dia - OK;
+ * Pegar horário da próxima fornada - OK;
+ * Comparar com a hora atual - OK;
+ * Criar timer regressivo até a próxima fornada - OK;
+ * Tocar alarme quando fornada pronta por 5s e a cada 30s por 5 minutos
  * Gravar no campo última fornada e iniciar timer novamente.
  * */
 
@@ -11,6 +11,10 @@ function cronometro() {
     // Descomentar linha abaixo para iniciar o cronometro na página.
 
     window.addEventListener('load', iniciaCron);
+    
+    // Array with hour and minute for bread batches
+    const batches = ["07:00","08:45","10:30","12:15","14:00","15:45","17:30","19:15","21:50"];
+    const initHour = new Date();
 	
     // Função de formatação dos segundos com base na data 0
 	function getHourFromSeconds(seconds) {
@@ -26,6 +30,21 @@ function cronometro() {
         return idLastBatch++
     };
 
+    // Function to determine the actual batch
+    function actualBatch(initHour){
+        let bakedBread = [];
+        for (let i = 0; i < batches.length; i++) {
+            const element = batches[i];
+            // Split de element to get the first position to compare with the initHour parameter
+            const arrayElement = element.split(':');
+            if (Number(arrayElement[0]) <= initHour){
+                bakedBread.push(arrayElement[0]);
+            }
+            
+        }
+        return bakedBread.length;
+    }
+
     // Function to determine seconds to the Next Batch of Bread
     function defineTimeToNextBatch(idNextBatch){
         const nextBatch = batches[idNextBatch];
@@ -38,16 +57,19 @@ function cronometro() {
     // Function to inform last batch of bread
     function lastBatchBread(idLastBatch){
         const lastBakedBread = document.querySelector('#lastBakedBread');
-        lastBakedBread.innerHTML = batches[idLastBatch-1];
+        if (idLastBatch < 9){
+            lastBakedBread.innerHTML = batches[idLastBatch-1];
+        } else {
+            lastBakedBread.innerHTML = '<p> Já encerramos as fornadas por hoje! <br> Agradecemos a preferencia! </p>'
+        }
+
     };
-    
-    // Array with hour and minute for bread batches
-    const batches = ["07:00","08:45","10:30","12:15","14:00","15:45","17:30","19:15","21:50"];
   
     // Variable Definitions
     const clock = document.querySelector('#timeToNextBread');
     let timeToWait = 5 * 60 * 1000; // 5 minutes
-	let seconds = Math.abs(defineTimeToNextBatch(4));
+    const batchesOk = actualBatch(initHour.getHours());
+	let seconds = Math.abs(defineTimeToNextBatch(batchesOk));
 	let timer;
 
     // Function for init timer
